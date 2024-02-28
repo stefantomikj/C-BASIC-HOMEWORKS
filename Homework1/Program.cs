3285c8dd0dbba4f629b4c3eb6b632ea35616935e
Stefan Tomik <stefantomic18@yahoo.com>
2024-02-28 17:30:12 +0100

    promena

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Homework1
+﻿using System;
+
+namespace Homework1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("    *    ");
+            Console.WriteLine("    *    ");
+            Console.WriteLine("  * * *  ");
+            Console.WriteLine("*   *   *");
+            Console.WriteLine("    *    ");
+            Console.WriteLine("    *    ");
+            Console.WriteLine("   * *    ");
+            Console.WriteLine("  *   *   ");
         }
     }
 }
