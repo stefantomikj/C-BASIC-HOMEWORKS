2106e14723d8f572acd528751607c17424062dd1
Stefan Tomik <stefantomic18@yahoo.com>
2024-03-01 23:14:02 +0100

    domasna2

---
+++ Homework2/SwapNumbers/SwapNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First number:");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Enter the Second number:");
+            string input2 = Console.ReadLine();
+
+            int number1;
+            bool success1 = int.TryParse(input1, out number1);
+
+            int number2;
+            bool success2 = int.TryParse(input2, out number2);
+
+            number1 = number1 + number2;
+            number2 = number1 - number2;
+            number1 = number1 - number2;
+            Console.WriteLine($"first number {number1}");
+            Console.WriteLine($"second number {number2}");
+        }
+    }
+}
