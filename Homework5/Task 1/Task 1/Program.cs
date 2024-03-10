c9acd4cb95d4b52cd5fbcf97cb61ec9299531656
Stefan Tomik <stefantomic18@yahoo.com>
2024-03-10 23:59:15 +0100

    domasna

---
+++ Homework5/Task 1/Task 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birthday (DD-MM-YYYY):");
+            string inputDate = Console.ReadLine();
+
+            int age = AgeCalculator(inputDate);
+            Console.WriteLine($"User age is: {age}");
+        }
+
+       public static int AgeCalculator(string inputBirthDay)
+        {
+            DateTime today = DateTime.Now;            
+            DateTime birthDate;
+
+            DateTime.TryParse(inputBirthDay, out birthDate);
+            int age = today.Year - birthDate.Year;
+            return age; 
+            
+
+        }
+    }
+}
+
+
---
+++ Homework5/Task2/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(" Please enter a number");
+                string input = Console.ReadLine(); 
+
+                if (int.TryParse(input, out int number))
+                {
+                    string phrase = "Hello from SEDC Codecademy 2024";
+                    string result = Substrings(phrase, number);
+
+                    Console.WriteLine($"First {number} characters: {result}");
+                    Console.WriteLine($"Length of the new string: {result.Length}");
+                    break;  
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+               
+            }
+        }
+
+        public static string Substrings(string inputPhrase, int num)
+        {
+            if (num >= 0 && num <= inputPhrase.Length)
+            {
+                return inputPhrase.Substring(0, num);
+            }
+            else
+            {
+                return "Number exceeds the length of the string.";
+            }
+        }
+    }
+}
