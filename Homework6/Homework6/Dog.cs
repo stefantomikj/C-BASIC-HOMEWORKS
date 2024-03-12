7dd95748da060d0faf4bf4e683488f115653005b
Stefan Tomik <stefantomic18@yahoo.com>
2024-03-12 20:49:58 +0100

    domasna

---
+++ Homework6/Homework6/Dog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }  
+        
+        public Dog(string nameInput, string breedInput, string colorInput) {
+        
+            Name = nameInput;
+            Breed = breedInput;
+            Color = colorInput;
+        }
+
+        public string Eat () {
+
+            return $"The {Name} is now eating";
+
+        }
+
+        public string Play()
+        {
+            return $"The {Name} is now playing.";
+
+        }
+
+        public string ChaseTail()
+        {
+
+            return $"The {Name} is now chasing it's tail.";
+        }
+    }
+
+}
---
+++ Homework6/Homework6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Homework6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter dog name");
+            string nameInput = Console.ReadLine();
+
+            Console.WriteLine("Enter dog breed");
+            string breedInput = Console.ReadLine();
+
+            Console.WriteLine("Enter dog color");
+            string colorInput = Console.ReadLine();
+
+            
+            Console.WriteLine("Please choise 1 or 2 or 3");
+            string choiseinput = Console.ReadLine();
+
+            Dog object1 = new Dog(nameInput, breedInput, colorInput);
+
+            while (true)
+            {
+
+                if (choiseinput == "1")
+                {
+                    Console.WriteLine(object1.Eat());
+                }
+                else if (choiseinput == "2")
+                {
+                    Console.WriteLine(object1.Play());
+
+                }
+                else if (choiseinput == "3")
+                {
+                    Console.WriteLine(object1.ChaseTail());
+
+                }
+                else
+                {
+                    Console.WriteLine("Wrong choise");
+                  
+                }
+                break;
+            }
+        }   
+    }
+}
