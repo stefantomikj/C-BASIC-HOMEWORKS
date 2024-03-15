f95db6d43a945552d10072e0b8c3b0b7cfdf7b68
Stefan Tomik <stefantomic18@yahoo.com>
2024-03-15 20:30:41 +0100

    domasna

---
+++ Homework7/Homework7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        NumberStats();
+    }
+
+    static void NumberStats()
+    {
+        
+
+        while (true)
+        {
+            Console.Write("Please enter a number: ");
+            string userInput = Console.ReadLine();
+
+            if (double.TryParse(userInput, out double number))
+            {
+                string numberType;
+                if (number % 1 == 0)
+                {
+                    numberType = "Integer";
+                }
+                else
+                {
+                    numberType = "Decimal";
+                }
+
+                string numberSign;
+                if (number > 0)
+                {
+                    numberSign = "Positive";
+                }
+                else if (number < 0)
+                {
+                    numberSign = "Negative";
+                }
+                else {
+                    numberSign = "Zero";
+                }
+           
+
+                string numberParity;
+                if (number % 2 == 0)
+                {
+                    numberParity = "Even";
+                }
+                else
+                {
+                    numberParity = "Odd";
+                }
+
+                Console.WriteLine($"Stats for number: {number}");
+                Console.WriteLine($"{numberSign}\n{numberType}\n{numberParity}");
+
+                Console.Write("Press Y to try again or X to exit: ");
+                string retry = Console.ReadLine();
+                if (retry.ToLower() == "y")
+                {
+                    continue;   
+                }
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter number");
+            }
+        }
+    }
+}
